0d387575323ce54d4edfef30d908b86477b85165
Eun Kang <choa030@gmail.com>
2020-08-30 22:18:32 +0900

    run build

--- src/index.js
+++ src/index.js
@@ -4,9 +4,11 @@ import * as Papa from "papaparse";
 // https://docs.google.com/spreadsheets/d/173OIZL1s1u0iD0m3swshseFh-z9tWDx9lPJHWVsROTQ/edit#gid=0
 
 var SHEET_ID = "173OIZL1s1u0iD0m3swshseFh-z9tWDx9lPJHWVsROTQ";
-var API_KEY = "AIzaSyD1SkHaZlfuGe-ltEQVnKqY18lQFhQ1m1Q";
+var API_KEY = "AIzaSyADT3iqNAWUl75iqvwuT1yKVN7dlew2EvI";
+
 
 function fetchSheet({ spreadsheetId, apiKey, complete }) {
+    console.log("dd");
     let url = `https://sheets.googleapis.com/v4/spreadsheets/${spreadsheetId}/values/roomKey/?key=${apiKey}`;
     return fetch(url).then(response =>
         response.json().then(result => {
@@ -19,6 +21,8 @@ function fetchSheet({ spreadsheetId, apiKey, complete }) {
 }
 
 function init() {
+    console.log("dd");
+
     fetchSheet({
         spreadsheetId: SHEET_ID,
         apiKey: API_KEY,
--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,17 @@
 module.exports = {
-    entry: __dirname + '/src/index.js',
+    entry: __dirname + "/src/index.js",
     output: {
-        path: __dirname + '/dist',
-        publicPath: '/dist/',
-        filename: 'bundle.js'
+        path: __dirname + "/dist",
+        publicPath: "/dist/",
+        filename: "bundle.js"
     },
     module: {
-        rules: [{
-            test: /\.js$/,
-            exclude: /node_modules/,
-            use: 'babel-loader'
-        }]
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: "babel-loader"
+            }
+        ]
     }
 };
